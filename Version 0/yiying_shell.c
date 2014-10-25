aa186fee4b76da703aa856b6181a2e9ccd84574c
danteWei <peterwei1057@hotmail.com>
2014-10-24 22:11:52 -0300

    Added two queues (historyRec and pipeRec)

--- Version 0/yiying_shell.c
+++ Version 0/yiying_shell.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "common.h"
 
+Queue historyRec;		// stores the history records
+Queue pipeRec;			// stores the commands (in order) in a pipe
 
 int main(int argc, char ** argv)
 {
